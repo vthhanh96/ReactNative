bdb36f79622116a1eabd52abde66eb4a3cc406e8
vanhongha <honghaa8@gmail.com>
2017-12-03 19:47:15 +0700

    Get all alarm list

--- DataController.js
+++ DataController.js
@@ -1,23 +1,71 @@
-import {AsyncStorage} from 'react-native';
+import {AsyncStorage,
+TouchableOpacity,
+View} from 'react-native';
 import React, {Component} from 'react';
-
-const alarmName = '1'
-const alarmObj = {latitude: 0, longitude: 0}
 
 export default class Data extends Component{
 
-  componentDidMount()
+  constructor()
   {
-    //AsyncStorage.setItem(alarmName, JSON.stringify(alarmObj));
-    AsyncStorage.getItem(alarmName)
-    .then((value) => {
-      const data = JSON.parse(value);
-      console.log('Gia tri la: ', data.latitude);
-    });
+    super();
+    this.state = {
+      alarmList: []
+    }
+  }
+
+  componentDidMount(){
+    this.loadAllAlarm();
+  }
+
+  callBackTest = () => {
+    for(var i = 0; i < this.state.alarmList.length; i++)
+      console.log(this.state.alarmList[i]);
+  }
+
+  loadAllAlarm()
+  {
+      AsyncStorage.getAllKeys()
+      .then(keys => {
+        console.log(keys.length);
+        console.log(this.state.isLoading);
+        this.getAllData(keys);
+      });
+  }
+
+  getAllData(keyArray)
+  {
+    var tempList = new Array();
+    AsyncStorage.multiGet(keyArray).then(
+      value => {
+        for(var i = 0; i < value.length; i++)
+        {
+          AsyncStorage.getItem(keyArray[i])
+          .then(itemValue => {
+            const objValue = JSON.parse(itemValue);
+            tempList.push(objValue);
+            this.setState({
+              alarmList: tempList
+            })
+          });
+        }
+      }
+    );
   }
 
   render()
   {
-    return null;
+    return(
+      <View
+        style={{ flex: 1 }}>
+        <TouchableOpacity
+          style={{ backgroundColor: "blue", flex: 0.5 }}
+          onPress = {this.callBackTest}
+        />
+        <TouchableOpacity
+          style={{ backgroundColor: "pink", flex: 0.5 }}
+          onPress = {this.callBackTest}
+        />
+      </View>
+    )
   }
 }
