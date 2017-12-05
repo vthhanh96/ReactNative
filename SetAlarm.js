9efde1fc6ccc3b5cac80ce6b2cf8189e5bf5a5aa
vthhanh <vthhanh96@gmail.com>
2017-12-05 22:33:37 +0700

    add navigation

---
+++ SetAlarm.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Text, View, Button} from 'react-native';
+import {StackNavigator} from 'react-navigation';
+
+class SetAlarm extends Component {
+
+	constructor(props) {
+		super(props);
+
+		const {params} = this.props.navigation.state;
+
+		this.state = {
+			alarm: {
+				latitude: params.latitude,
+				longitude: params.longitude,
+				minDisToAlarm: 100,
+			}
+		}
+	}
+
+	static navigationOptions = {
+	    title: 'Alarm Settings',
+	    headerRight: <Button 
+	    				title="Save"
+	    				onPress={() => console.log("Click save button")}/>
+	};
+
+
+	render() {
+		return(
+			<View style={{padding: 20, paddingTop: 100}}>
+			<Text style={{fontSize: 30}}>
+				Latitude: {this.state.alarm.latitude}
+			</Text>
+			<Text style={{fontSize: 30}}>
+				Longitude: {this.state.alarm.longitude}
+			</Text>
+			<Text style={{fontSize: 30}}>
+				Min distance to alarm: {this.state.alarm.minDisToAlarm}m
+			</Text>
+			</View>
+		)
+	}
+}
+
+export default SetAlarm;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import Map from './Map';
 import AlarmScene from './Alarm';
 import GeolocationTracking from './TrackingLocation';
 import GPlacesDemo from './SearchPlace';
